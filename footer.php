87dd1c1c05da8ec51de17889305dbe10d61be723
Mykola Dudchak <nik35092@gmail.com>
2023-01-26 11:15:00 +0200

    WP-SITE

---
+++ footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+    <div class="container">
+      <div class="footer__inner">
+        <?php the_custom_logo();?>
+
+        <div class="social footer__social">
+          <a class="social__link" href="<?php the_field('instagram-link');?>">
+              <svg class="test" width="26" height="26">
+                <use xlink:href="<?php bloginfo('template_url');?>/assets/images/icon/sprite.svg#instagram"></use>
+              </svg>
+            </a>
+            <a class="social__link" href="<?php the_field('telegram-link');?>">
+              <svg width="25" height="19">
+                <use xlink:href="<?php bloginfo('template_url');?>/assets/images/icon/sprite.svg#telegram"></use>
+              </svg>
+            </a>
+            <a class="social__link" href="<?php the_field('whatsup-link');?>">
+              <svg width="26" height="26">
+                <use xlink:href="<?php bloginfo('template_url');?>/assets/images/icon/sprite.svg#whatsapp"></use>
+              </svg>
+            </a>
+            <a class="social__link" href="<?php the_field('facebook-link');?>">
+              <svg width="14" height="25">
+                <use xlink:href="<?php bloginfo('template_url');?>/assets/images/icon/sprite.svg#facebook"></use>
+              </svg>
+            </a>
+        </div>
+
+        <a class="footer__copy" href="#">
+          Политика конфиденциальности
+        </a>
+      </div>
+    </div>
+  </footer>
+
+  <?php wp_footer();?>
+</body>
+
+</html>
